10c9565d1594de605c2961412f9eff14d6afdb6a
Eduardo Macedo <eduardomacedo.eng@hotmail.com>
2023-07-30 14:31:18 -0300

    Configure RabbitMQ to use JSON and create POJO Class to Serialize/Deserialize

--- src/main/java/br/com/macedo/springboot/config/RabbitMQConfig.java
+++ src/main/java/br/com/macedo/springboot/config/RabbitMQConfig.java
@@ -1,9 +1,10 @@
 package br.com.macedo.springboot.config;
 
-import org.springframework.amqp.core.Binding;
-import org.springframework.amqp.core.BindingBuilder;
-import org.springframework.amqp.core.Queue;
-import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.core.*;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -14,16 +15,28 @@ public class RabbitMQConfig {
     @Value("${rabbitmq-params.queue-name}")
     private String queue;
 
+    @Value("${rabbitmq-params.queue-name-json}")
+    private String jsonQueue;
+
     @Value("${rabbitmq-params.exchange-name}")
     private String exchange;
 
     @Value("${rabbitmq-params.routing-key-name}")
     private String routingKey;
 
+    @Value("${rabbitmq-params.routing-key-name-json}")
+    private String routingKeyJson;
+
     //TODO: Spring Bean for RabbitMQ Queue
     @Bean
     public Queue queue() {
         return new Queue(queue);
+    }
+
+    //TODO: Spring Bean for Queue (store json messages)
+    @Bean
+    public Queue queueJson() {
+        return new Queue(jsonQueue);
     }
 
     //TODO: Spring Bean for RabbitMQ Exchange
@@ -41,9 +54,31 @@ public class RabbitMQConfig {
                 .with(routingKey);
     }
 
+    //TODO: Biding between queueJson and exchange using routing key
+    @Bean
+    public Binding bindingJson() {
+        return BindingBuilder
+                .bind(queueJson())
+                .to(exchange())
+                .with(routingKeyJson);
+    }
+
     //TODO: OBS -> When we use Bean annotation the springboot autoconfigure these follow components for us:
     // 1. ConnectionFactory
     // 2. RabbitTemplate
     // 3. RabbitAdmin
+
+    @Bean
+    public MessageConverter converter() {
+        return new Jackson2JsonMessageConverter();
+    }
+
+    @Bean
+    public AmqpTemplate amqpTemplate(ConnectionFactory connectionFactory) {
+        RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+        rabbitTemplate.setMessageConverter(converter());
+
+        return rabbitTemplate;
+    }
 
 }
